f4b6be8b4b1f92a584bfd260188835cf9341a3dc
Dulguun <dulguun@gyalstugrug.local>
2024-06-18 16:12:13 +0800

    update2

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-  basePath: "/Manage-landing-page", // replace 'my-nextjs-app' with your repo name
-  assetPrefix: "/Manage-landing-page/",
-};
-
 export default nextConfig;
